d8d4a149aacbaed177a458ccbc12e3e64f8d161f
Bruno Profeta <brunoprofetacavalcante@gmail.com>
2024-11-01 11:41:21 -0300

    Initial Commit

---
+++ src/test/java/com/fiap/springdatamongodb/SpringdatamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
